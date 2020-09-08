50b57446d6c0472beb4cc66c746ea779c51f6e8e
Ismail Hartley <Ismailhartley1@gmail.com>
2020-09-07 18:20:28 -0600

    iteration 1 almost complete

--- main.js
+++ main.js
@@ -1,1 +1,52 @@
+//QUERY SELECTORS:
+var letsCookButton = document.querySelector(".lets-cook-button");
+var cookPotIcon = document.querySelector(".cook-pot-icon");
+var resultBox = document.querySelector(".result-box");
+var select = document.querySelectorAll(".select");
 
+var currentDish;
+
+//EVENT LISTENERS:
+letsCookButton.addEventListener("click", displayDish);
+
+
+//FUNCTIONS:
+
+function getRandomIndex(array){
+  return Math.floor(Math.random() * array.length);
+}
+
+function chooseSide(){
+  currentDish = sides[getRandomIndex(sides)];
+}
+
+function chooseMains(){
+  currentDish = mains[getRandomIndex(mains)];
+}
+
+function chooseDessert(){
+  currentDish = desserts[getRandomIndex(desserts)];
+}
+
+function randomizeDish() {
+  var randomSideDish = chooseSide();
+  var randomMainDish = chooseMains();
+  var randomDessert = chooseDessert();
+  if (select[0].checked) {
+    displayDish(randomSideDish);
+  } else if (select[1].checked) {
+    displayDish(randomMainDish);
+  } else if (select[2].checked) {
+    displayDish(randomDessert);
+  } else if (select[3].checked) {
+    displayMeal(randomSideDish, randomMainDish, randomDessert);
+  }
+}
+
+function displayDish(){
+  cookPotIcon.classList.add("hidden");
+  resultBox.insertAdjacentHTML("afterbegin", `
+    <a>You should make:</a>
+    <a class = "statement">${currentDish}!</a>
+  `);
+}
